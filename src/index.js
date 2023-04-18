c8b8b26bfdc3b66629dc38e9210991760d096725
Armandocres <armandocres99@gmail.com>
2023-04-18 14:24:58 -0600

    creando imagenes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const mountNOde = document.querySelector('#images');
+const addButton = document.querySelector('button');
 
-console.log('Happy hacking :)')
+const maximum = 122;
+const minimum = 1;
+
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createIMageNOde = () => {
+  const container = document.createElement('div');
+  container.className = 'p-4';
+
+  const image = document.createElement('img');
+  image.className= ('mx-auto');
+  image.width = '320';
+  image.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  container.appendChild(image)
+
+  return container;
+}
+
+const addIMage = () => {
+  const newImage = createIMageNOde();
+  mountNOde.appendChild(newImage);
+}
+
+addButton.addEventListener('click', addIMage);
